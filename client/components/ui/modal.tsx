68ac5f373c7574dece722e1cd913b53b887afe93
Leo Villalobos <villaleobos@gmail.com>
2025-06-27 11:49:24 -0700

    client(nit): blur bg when creating new ticket

--- client/components/ui/modal.tsx
+++ client/components/ui/modal.tsx
@@ -10,8 +10,8 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="bg-gray-50 dark:bg-gray-900 p-6 rounded-lg shadow-lg w-full max-w-lg relative">
+    <div className="fixed backdrop-blur-sm backdrop-opacity-90 inset-0 z-50 flex items-center justify-center">
+      <div className="bg-gray-50 dark:bg-gray-900 p-1 rounded-lg shadow-lg w-full max-w-lg relative">
         <button
           onClick={onClose}
           className="absolute hover:cursor-pointer top-2 right-2 text-gray-500 dark:text-gray-300 hover:text-red-500 text-xl font-bold"
