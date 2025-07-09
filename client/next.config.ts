6bc5442a44a9d2adee76b4c2d233dde276bd90fb
Leo Villalobos <villaleobos@gmail.com>
2025-07-09 13:17:42 -0700

    client(fix): provide a url glob under images.remotePatterns for known image hosts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // TODO: Resolve hostname instead of direct link to image
-    remotePatterns: [new URL("https://lh3.googleusercontent.com/a/ACg8ocJnIHFbpxBJNZ7Y_dTJDCAbfjfgqCTGi5Xd9myR7x1LsOzwwFll=s96-c")]
-  }
+    remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
+  },
 };
 
 export default nextConfig;
