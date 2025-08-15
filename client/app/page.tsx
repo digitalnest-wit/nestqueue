a46fa90320f6ef54b0a4dc3802c11b8fdddebb1e
Christian Rodrigues <Christianrodrigues02@gmail.com>
2025-08-15 14:41:59 -0700

    implement user authentication flow in Home component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,15 @@
 "use client";
 import LoginPage from "@/app/login/page";
+import { useAuth } from "@/lib/context/auth";
+import TicketsPage from "@/app/(protected)/tickets/page";
 
 export default function Home() {
-  return <LoginPage />;
+  const { user } = useAuth();
+
+
+  if (typeof user === "undefined") {
+    return <div>Loading...</div>;
+  }
+
+  return user ? <TicketsPage /> : <LoginPage />;
 }
